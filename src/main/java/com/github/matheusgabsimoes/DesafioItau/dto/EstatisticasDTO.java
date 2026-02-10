e2973bf41140a12d85782ca2315e9a6902e7296f
Matheus Simões <mtsgabriel.simoes@gmail.com>
2026-02-09 21:01:55 -0300

    feat: DTO das Estatistica

---
+++ src/main/java/com/github/matheusgabsimoes/DesafioItau/dto/EstatisticasDTO.java
@@ -0,0 +1,9 @@
+package com.github.matheusgabsimoes.DesafioItau.dto;
+
+public record EstatisticasDTO(Long count,
+                              Double sum,
+                              Double avg,
+                              Double min,
+                              Double max) {
+
+}
